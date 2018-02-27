d8dace02b545bb6268726fb87c692e89e78b9f35
Anderson Pessoa <anderson.pessoa002@gmail.com>
2018-02-27 16:06:58 -0300

    Implement Blockchain structure

---
+++ BlockChain/Structure/Block.cs
@@ -0,0 +1,60 @@
+﻿using BlockChain.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BlockChain.Structure
+{
+    public class Block<T>
+    {
+        public String Hash { get; set; }
+        public String PreviousHash { get; set; }
+        public T Data { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Difficulty { get; set; }
+        public int Nonce { get; set; }
+
+        public Block(string previousHash, T data, int difficulty = 1)
+        {
+            this.PreviousHash = previousHash;
+            this.Data = data;
+            this.CreatedAt = DateTime.Now;
+            this.Difficulty = difficulty;
+            this.Nonce = 0;
+
+            this.Mine();
+        }
+
+        public String GenerateSHA256()
+        {
+            var stringBlock = Newtonsoft.Json.JsonConvert.SerializeObject(this.Data) + this.CreatedAt + this.Difficulty + this.Nonce;
+            var bytes = new SHA256Managed().ComputeHash(Encoding.ASCII.GetBytes(stringBlock));
+
+            return bytes.ToBase64();
+        }
+
+        private bool Mine()
+        {
+            this.Hash = this.GenerateSHA256();
+
+            while (!Regex.IsMatch(this.Hash.Substring(0, this.Difficulty), "^(0){" + this.Difficulty + "}"))
+            {
+                try
+                {
+                    this.Nonce++;
+                    this.Hash = this.GenerateSHA256();
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ BlockChain/Structure/BlockChain.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Internal.System.Collections.Sequences;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockChain.Structure
+{
+    public class BlockChain<T>
+    {
+        public List<Block<T>> Blocks { get; set; }
+        public int Difficulty { get; set; }
+
+
+        public BlockChain(Block<T> block)
+        {
+            this.Blocks = new List<Block<T>>();
+            this.Difficulty = block.Difficulty;
+            this.Blocks.Add(block);
+        }
+
+        public Block<T> GetLastBlock()
+        {
+            return Blocks.LastOrDefault();
+        }
+
+        public void AddBlock(T data)
+        {
+            var lastBlock = this.GetLastBlock();
+
+            var newBlock = new Block<T>(lastBlock.PreviousHash, data, 2);
+
+            Blocks.Add(newBlock);
+        }
+
+
+        public bool IsValid()
+        {
+            for (int i = 1; i <= this.Blocks.Count; i++)
+            {
+                var currentBlock = this.Blocks.ElementAt(i);
+                var previousBlock = this.Blocks.ElementAt(i - 1);
+
+                if (currentBlock.PreviousHash != previousBlock.Hash) return false;                                
+            }
+
+            return true;
+        }
+    }
+}
