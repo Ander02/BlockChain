ad622ffcc02f4ac7fb1d27a7e428535f1d9cd6ce
Anderson Pessoa <anderson.pessoa002@gmail.com>
2018-03-05 10:59:55 -0300

    Uptade program

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -21,8 +21,17 @@ namespace BlockChain
 
             Console.WriteLine("Digite o valor do bloco Gênesis");
             Console.Write(">");
+
+            var tInicio = DateTime.Now;
             var genesis = new Block<string>(Console.ReadLine(), difficulty);
             var chain = new BlockChain<string>(genesis);
+            var tFim = DateTime.Now;
+
+            Console.WriteLine("O bloco gênesis é: ");
+            chain.GetGenesisBlock().PrintBlock();
+            Console.WriteLine();
+            Console.WriteLine("Esse bloco demorou {0} ms para ser criado: ", (tFim - tInicio).Milliseconds);
+            Console.WriteLine();
 
             int opcao = 0;
             do
@@ -37,18 +46,25 @@ namespace BlockChain
                 Console.WriteLine("0 - Sair");
                 Console.Write(">");
 
-                if (!int.TryParse(Console.ReadLine(), out opcao)) opcao = -1;
+                if (!Int32.TryParse(Console.ReadLine(), out opcao)) opcao = -1;
 
                 switch (opcao)
                 {
                     case 1:
-                        Console.WriteLine("Digite os dados do bloco");
+                        Console.WriteLine("Digite o valor do bloco");
                         Console.Write(">");
+
+                        tInicio = DateTime.Now;
                         chain.AddBlock(Console.ReadLine());
+                        tFim = DateTime.Now;
+
                         Console.WriteLine();
                         Console.WriteLine("O bloco inserido foi: ");
                         chain.GetLastBlock().PrintBlock();
                         Console.WriteLine();
+                        Console.WriteLine("Esse bloco demorou {0} ms para ser criado: " , (tFim - tInicio).Milliseconds);
+                        Console.WriteLine();
+
                         break;
                     case 2:
                         Console.WriteLine("O último bloco é: ");
